8a36992893a812041e8c7129590762ee847b7a36
jozef-thomas <52873243+jozef-thomas@users.noreply.github.com>
2024-04-01 15:45:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// console.log("hello")
+
+// // var,let,const
+
+// var name = "joseph"
+// console.log(name)
+
+ var a=5
+ var b=10
+ if(a>b){
+    console.log("a is greater")
+ }
+ else{
+    console.log("b is greater")
+ }
+ 
